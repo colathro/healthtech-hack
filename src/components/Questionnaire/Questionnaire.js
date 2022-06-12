1bb3b751b907383eeddf14810d045022b3460baa
Colton <42881380+colathro@users.noreply.github.com>
2022-06-11 19:49:56 -0700

    depression, ptsd, and wired up progress (#5)

--- src/components/Questionnaire/Questionnaire.js
+++ src/components/Questionnaire/Questionnaire.js
@@ -8,7 +8,10 @@ import Question from "../Question/Question";
 const Questionnaire = observer(() => {
   return (
     <div className="questionnaire-page">
-      <ProgressBar bgcolor="green" completed={50}/>
+      <ProgressBar
+        bgcolor="green"
+        completed={questionnaireState.calculateProgress()}
+      />
       <div className="questionnaire-page-header">
         Questionnaire goes here!
         {questionnaireState.questions.map((question) => {
--- src/state/InitialQuestions.js
+++ src/state/InitialQuestions.js
@@ -18,6 +18,44 @@ const InitialQuestions = [
     followUpSignal: true, // when this value matches the answer, follow up questions are displayed
     answer: null, // initial answer to the question, type property defines what goes here
   },
+  {
+    type: "yesno",
+    text: "Have you been feeling down, hopeless, tired, or the lack of pleasure?",
+    followUp: true,
+    followUpQuestions: [
+      {
+        type: "frequency",
+        text: "How often do you feel down, hopeless, or tired?",
+        answer: null,
+      },
+      {
+        type: "intensity",
+        text: "How intense are the feelings of being down, hopeless, or tired?",
+        answer: null,
+      },
+    ],
+    followUpSignal: true,
+    answer: null,
+  },
+  {
+    type: "yesno",
+    text: "How much have you had strong physical reactions when something reminded you of the stressful experience (for example, heart pounding, trouble breathing, sweating)?",
+    followUp: true,
+    followUpQuestions: [
+      {
+        type: "frequency",
+        text: "How often do you feel these strong physical reactions?",
+        answer: null,
+      },
+      {
+        type: "intensity",
+        text: "How intense are the physical reactions when something reminded you of the stressful experience?",
+        answer: null,
+      },
+    ],
+    followUpSignal: true,
+    answer: null,
+  },
 ];
 
 export default InitialQuestions;
