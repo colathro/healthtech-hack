d8f2078c5d43de3500ea10060df993d2d6c496e8
Ariana <96448173+ammartens@users.noreply.github.com>
2022-06-12 01:28:41 -0700

    results page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -19,28 +19,30 @@ function LandingPage() {
             <p className="hero-txt">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris added-features
-              nisi ut aliquip ex ea commodo consequat.
-
-              nisi ut aliquip ex ea commodo consequat. main
-              Millions of people stuggle daily with mental health disorders, and
-              lack the resources to easily find where to start. <br /> <br />
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              added-features nisi ut aliquip ex ea commodo consequat. nisi ut
+              aliquip ex ea commodo consequat. main Millions of people stuggle
+              daily with mental health disorders, and lack the resources to
+              easily find where to start. <br /> <br />
               Take our short questionnaire to get some initial direction and
               resources to start your treatment journey.
             </p>
             <button className="hero-button" onClick={questionnaire}>
-              Start Questionnaire >
+              Start Questionnaire
             </button>
           </div>
         </div>
-        <div className="hero-right-container"> added-features
         <div className="hero-right-container">
-          <img
-            className="imgContainer"
-            src="./humans/twonewhumans.svg"
-            alt=""
-          />
-          <img className="imgContainer" src="humans/twohumans.svg" alt="" /> main
+          {" "}
+          added-features
+          <div className="hero-right-container">
+            <img
+              className="imgContainer"
+              src="./humans/twonewhumans.svg"
+              alt=""
+            />
+            <img className="imgContainer" src="humans/twohumans.svg" alt="" />
+          </div>
         </div>
       </div>
     </div>
